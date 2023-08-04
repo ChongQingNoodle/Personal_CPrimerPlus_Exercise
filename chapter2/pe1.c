e93016b05b5946b0e092fba46d96baeb6893592e
ChongQingNoodle <chia.0@outlook.com>
2023-08-04 17:39:40 +0800

    programming exercise 1

---
+++ chapter2/pe1.c
@@ -0,0 +1,13 @@
+// 1.编写一个程序,调用一次printf()函数,把你的名和姓打印在一行。再调用一次printf()函
+// 数,把你的名和姓分别打印在两行。然后,再调用两次printf()函数,把你的名和姓打印在一
+// 行。输出应如下所示(当然要把示例的内容换成你的名字):
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Jia Ziyan\n");
+    printf("Ziyan\nJia\n");
+    printf("Jia ");
+    printf("Ziyan\n");
+}
